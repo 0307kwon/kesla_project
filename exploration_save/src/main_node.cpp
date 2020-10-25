4477acce8f9fefda26b8acda4ca98ffe6e4409ff
0307kwon <0307kwon@gmail.com>
2020-10-25 22:04:13 +0900

    서비스 부분 추가

--- exploration_save/src/main_node.cpp
+++ exploration_save/src/main_node.cpp
@@ -1,5 +1,29 @@
 #include <ros/ros.h>
+#include <kesla_msg/DoneService.h>
+#include <string>
 
-int main(){
+using namespace std;
+bool srv_callback(kesla_msg::DoneService::Request &req,
+                  kesla_msg::DoneService::Response &res)
+{
+  cout << req.myRequest <<endl;
+  if(req.myRequest.compare("finished") == 0){
+    res.myResponse = "success";
+  }else{
+    res.myResponse = "fail";
+  }
+  return true;
+}
+
+
+int main(int argc, char** argv){
+
+  ros::init(argc,argv,"main_node");
+
+  ros::NodeHandle n;
+  ros::ServiceServer client = n.advertiseService("explore_server/sendExplorDone",srv_callback);
+  ROS_INFO("Be ready");
+  ros::spin();
+
   return 0;
 }
