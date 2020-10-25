9ee69f7c6f54fc6ce61e3f21535e9b1463cdd25d
장성광 <rbd4562@naver.com>
2020-10-25 22:21:04 +0900

    성광

--- exploration_save/src/main_node.cpp
+++ exploration_save/src/main_node.cpp
@@ -1,5 +1,18 @@
 #include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
 
-int main(){
-  return 0;
+void msgCallback(const nav_msgs::Odometry::ConstPtr& msg){
+  std::cout << "x:" << msg->pose.pose.position.x << std::endl;
+
+}
+
+int main(int argc, char** argv){
+
+  ros::init(argc, argv, "exploration_save");
+  ros::NodeHandle nh;
+
+  ros::Subscriber sub = nh.subscribe<nav_msgs::Odometry>("/odom",10,msgCallback);
+
+  ros::spinOnce();
+
 }
