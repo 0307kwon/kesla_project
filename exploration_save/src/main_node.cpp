05699ef5b183a16d60180a9fa740b4dcd577e655
장성광 <rbd4562@naver.com>
2020-10-29 20:52:34 +0900

    odom_start 저장1

--- exploration_save/src/main_node.cpp
+++ exploration_save/src/main_node.cpp
@@ -2,6 +2,12 @@
 #include <nav_msgs/Odometry.h>
 #include <kesla_msg/DoneService.h>
 #include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#define MAX_SIZE 1000
+char inputString[MAX_SIZE];
 
 
 using namespace std;
@@ -17,13 +23,26 @@ bool srv_callback(kesla_msg::DoneService::Request &req,
   return true;
 }
 
-void msgCallback(const nav_msgs::Odometry::ConstPtr& msg){
+void makeTextfile(const char* myString){
+  //myString의 변수를 file의 한 줄에 추가//
+  ofstream outFile("odom_start.txt");
+  outFile << myString << endl;
 
+  outFile.close();
+}
+
+
+void msgCallback(const nav_msgs::Odometry::ConstPtr& msg){
+//nav_msgs 토픽의 Odometry 메세지를 받음.
   std::cout << "x:" << msg->pose.pose.position.x << std::endl;
   std::cout << "y:" << msg->pose.pose.position.y << std::endl;
   std::cout << "z:" << msg->pose.pose.position.z << std::endl;
+  stringstream ss;
+  ss << "odom_start:"<< msg->pose.pose.position.x << "," << msg->pose.pose.position.y << std::endl;
 
+  makeTextfile(ss.str().c_str());
 }
+
 int main(int argc, char** argv){
 
   ros::init(argc, argv, "exploration_save");
